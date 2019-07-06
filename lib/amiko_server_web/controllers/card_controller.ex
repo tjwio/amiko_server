738a64a52af2774f93124a0555607c558d43fa13
Tim Wong <tim@tjw.io>
2019-07-05 20:09:28 -0700

    add post /cards

--- lib/amiko_server_web/controllers/card_controller.ex
+++ lib/amiko_server_web/controllers/card_controller.ex
@@ -1,7 +1,7 @@
 defmodule AmikoServerWeb.CardController do
   use AmikoServerWeb, :controller
 
-  alias AmikoServer.{Accounts, Authentication.Guardian, Connections, Repo}
+  alias AmikoServer.{Accounts, Authentication.Guardian, Repo}
 
   action_fallback AmikoServerWeb.FallbackController
 
@@ -25,6 +25,20 @@ defmodule AmikoServerWeb.CardController do
       profession: card.user.profession,
       company: card.user.company,
       image_url: card.user.image_url }))
+  end
+
+  def add_card(conn, params) do
+    user = Guardian.Plug.current_resource(conn)
+    params = Map.put(params, "user_id", user.id)
+
+    case Accounts.create_card(params) do
+      {:ok, card} ->
+        conn
+        |> send_resp(200, Poison.encode!(card))
+      {:error, _changeset} ->
+        conn
+        |> send_resp(400, Poison.encode!(%{message: "Failed to create card"}))
+    end
   end
 
 end
